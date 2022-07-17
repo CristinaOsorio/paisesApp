3ea2c6483c08f6e4ed349df39b1146db46da6a82
Cristina Osorio <48217224+CristinaOsorio@users.noreply.github.com>
2022-07-17 16:02:25 -0500

    Diseño y funcionamiento de la vista by-region actualizacion redireccion a la ruta pais/:id de country-table

--- src/app/pais/pages/by-region/by-region.component.ts
+++ src/app/pais/pages/by-region/by-region.component.ts
@@ -1,3 +1,5 @@
+import { Country } from './../../interfaces/country.interface';
+import { PaisService } from './../../services/pais.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class ByRegionComponent implements OnInit {
+  regions: string[] = [
+    'americas',
+    'africa',
+    'asia',
+    'europe',
+    'oceania'
+  ];
+  activeRegion: string = '';
+  countries: Country[] = [];
 
-  constructor() { }
+  constructor(
+    private paisService:PaisService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  regionActivate(region: string) {
+    if (region == this.activeRegion) return;
+
+    this.activeRegion = region;
+    this.countries = [];
+    this.paisService.searchRegion(region).subscribe(countries => {
+      this.countries = countries;
+    })
+  }
+
+  getClassBtn(region: string ): string {
+    return  region === this.activeRegion ?
+      'btn-primary' : 
+      'btn-outline-primary';
+    
   }
 
 }
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,7 +1,8 @@
 
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 import { Country } from './../interfaces/country.interface';
 
@@ -11,21 +12,27 @@ import { Country } from './../interfaces/country.interface';
 export class PaisService {
   private apiUrl: string = 'https://restcountries.com/v3.1';
 
+  get httpParams() {
+    return  new HttpParams().set('fields', 'name,population,flags,cca2,capital');
+  }
+
   constructor(private http: HttpClient) { }
 
   searchCountry(word: string): Observable<Country[]> {
     const url = `${this.apiUrl}/name/${word}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, { params: this.httpParams }).pipe(
+      tap(console.log)
+    );
   }
 
   searchCapital(word: string) {
     const url = `${this.apiUrl}/capital/${word}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, { params: this.httpParams });
   }
 
   searchRegion(word: string) {
     const url = `${this.apiUrl}/region/${word}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, { params: this.httpParams });
   }
 
   getCountryByAlpha(id: string): Observable<Country[]> {
